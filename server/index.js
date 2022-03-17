8abec657c99504a982f58e36c51c2ab4cf6c1c0d
busrasss <busrates.89@gmail.com>
2022-03-17 22:15:09 +0300

    added server.js

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app= express();
+const mongoose = requite('mongoose')
+
+mongoose.connect("mongodb+srv://user123:Password123@cluster0.4y63l.mongodb.net/mern60minutes?retryWrites=true&w=majority")
+
+
+app.listen(3001, () => {
+    console.log("SERVER RUNS PERFECTLY!");
+});
