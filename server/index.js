4812ec4bf89d392185a30324a701b9d9233481ae
busrasss <busrates.89@gmail.com>
2022-03-17 22:35:52 +0300

    added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app= express();
-const mongoose = requite('mongoose')
+const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://user123:Password123@cluster0.4y63l.mongodb.net/mern60minutes?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://user123:Password123@cluster0.4y63l.mongodb.net/mern60minutes?retryWrites=true&w=majority");
 
 
 app.listen(3001, () => {
