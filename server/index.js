78c143ebeac81a56402e17dd8f654ebfe728b6a2
busrasss <busrates.89@gmail.com>
2022-03-18 21:11:46 +0300

    added get and post

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const app= express();
 const mongoose = require('mongoose');
 const UserModel = require("./models/Users");
 
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
 mongoose.connect("mongodb+srv://user123:Password123@cluster0.4y63l.mongodb.net/mern60minutes?retryWrites=true&w=majority");
 
 app.get("/getUsers", (req, res) => {
@@ -14,6 +19,16 @@ app.get("/getUsers", (req, res) => {
         }
     });
 });
+
+app.post("/createUser", async(req,res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+
+    res.json(user);
+});
+
 app.listen(3001, () => {
     console.log("SERVER RUNS PERFECTLY!");
 });
