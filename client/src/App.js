438eb10da93356c97843a802829e5a18d52fc63d
busrasss <busrates.89@gmail.com>
2022-03-19 19:23:37 +0300

    last changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,31 @@ import Axios from "axios";
 function App() {
 
   const [listOfUsers, setListOfUsers] = useState([ ]);
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [username, setUsername] = useState("")
 
   useEffect (() => {
       Axios.get("http://localhost:3001/getUsers").then((response) => {
         setListOfUsers(response.data);
       });
   }, []);
+
+  const createUser = () => {
+    Axios.post("http://localhost:3001/createUser", {
+      name,
+      age,
+      username,
+    }).then((response) => {
+      setListOfUsers([...listOfUsers, 
+        {
+          name,
+          age,
+          username
+        
+        }])
+   });
+  };
 
   return (
     <div className="App">
@@ -26,6 +45,27 @@ function App() {
           );
         })}
 
+      </div>
+      <div>
+        <input type="text"
+         placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }} 
+          />
+        <input type="number"
+         placeholder="Age..."
+         onChange={(event) => {
+          setAge(event.target.value);
+        }}  
+         />
+        <input type="text" 
+          placeholder="Username..." 
+          onChange={(event) => {
+          setUsername(event.target.value);
+        }} 
+        />
+        <button onClick={createUser}>Create User</button>
       </div>
       
     </div>
